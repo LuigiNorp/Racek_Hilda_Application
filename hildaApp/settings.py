16ace91eb6c6d57cce85cfedeb13797d7ccb05a9
Jose Luis López López Prieto <ing.jlllopezp@gmail.com>
2023-07-13 01:16:53 -0600

    Added settings.py MEDIA_URL

--- hildaApp/settings.py
+++ hildaApp/settings.py
@@ -198,4 +198,5 @@ X_FRAME_OPTIONS = 'SAMEORIGIN'
 DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 
 # to use SMB
-MEDIA_ROOT = '/app/media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
