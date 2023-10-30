53d8cb2bb2e8ae8ba211e2242656f0bd468193eb
Jose Luis López López Prieto <ing.jlllopezp@gmail.com>
2023-10-30 10:18:00 -0600

    Added changes to views.py in order to acommodate the functions and methods of generate_DC3 view

--- main/views.py
+++ main/views.py
@@ -22,6 +22,50 @@ from .forms import *
 from .models import *
 
 
+def keep_first_page(pdf_path):
+    # Open the PDF file
+    reader = PdfReader(pdf_path)
+
+    # Create a new PDF writer
+    writer = PdfWriter()
+
+    # Add the first page to the writer
+    writer.add_page(reader.pages[0])
+
+    # Write the output to a new file
+    with open(pdf_path, 'wb') as output_file:
+        writer.write(output_file)
+
+
+def verify_data(personal, datos):
+    result = {}
+    for key, function in datos.items():
+        try:
+            result[key] = function(personal)
+        except AttributeError:
+            result[key] = None
+        except ValueError:
+            result[key] = 'Invalid value'
+    return result
+
+
+def scale_image_from_height(image_path, desired_height_cm):
+    # Load the image with PIL to get its size
+    with PilImage.open(image_path) as pil_img:
+        original_width, original_height = pil_img.size
+
+    # Convert the height from cm to pixels and set the height of the image
+    dpi = 96
+    cm_to_pixels = lambda cm: int(dpi * cm / 2.54)  # convert cm to pixels
+    desired_height_px = cm_to_pixels(desired_height_cm)
+
+    # Calculate the new width to maintain aspect ratio
+    scale_factor = desired_height_px / original_height
+    desired_width_px = int(original_width * scale_factor)
+
+    return desired_width_px, desired_height_px
+
+
 # Create your views here.
 class LoginView(View):
     template_name = 'login/login.html'
@@ -343,39 +387,14 @@ class UserHistoryView(LoginRequiredMixin, TemplateView):
         return render(request, self.template_name, context)
 
 
+def remove_accents(text):
+    # Use unidecode to remove accents and replace special characters
+    return unidecode(text)
+
+
 class GenerateDC3View(View):
-    def remove_accents(self, text):
-        # Use unidecode to remove accents and replace special characters
-        return unidecode(text)
-
-    def verify_data(self, personal, datos):
-        result = {}
-        for key, function in datos.items():
-            try:
-                result[key] = function(personal)
-            except AttributeError:
-                result[key] = None
-            except ValueError:
-                result[key] = 'Invalid value'
-        return result
-
-    def scale_image_from_height(self, image_path, desired_height_cm):
-        # Load the image with PIL to get its size
-        with PilImage.open(image_path) as pil_img:
-            original_width, original_height = pil_img.size
-
-        # Convert the height from cm to pixels and set the height of the image
-        dpi = 96
-        cm_to_pixels = lambda cm: int(dpi * cm / 2.54)  # convert cm to pixels
-        desired_height_px = cm_to_pixels(desired_height_cm)
-
-        # Calculate the new width to maintain aspect ratio
-        scale_factor = desired_height_px / original_height
-        desired_width_px = int(original_width * scale_factor)
-
-        return desired_width_px, desired_height_px
-
-    def add_image_to_worksheet(self, image_path, cell, activesheet, width, height):
+    @staticmethod
+    def add_image_to_worksheet(image_path, cell, activesheet, width, height):
         # Load and add the image with openpyxl
         img = XlsxImage(image_path)
         img.width = width
@@ -383,25 +402,12 @@ class GenerateDC3View(View):
 
         # Add image to the specified cell
         activesheet.add_image(img, cell)
-
-    def convert_xlsx_to_pdf(self, xlsx_path, pdf_path):
+    
+    @staticmethod
+    def convert_xlsx_to_pdf(xlsx_path, pdf_path):
         convertion_command = f"libreoffice --headless --convert-to pdf:writer_pdf_Export --outdir {os.path.dirname(pdf_path)} {xlsx_path}"
         subprocess.run(convertion_command, shell=True)
         return pdf_path
-
-    def keep_first_page(self, pdf_path):
-        # Open the PDF file
-        reader = PdfReader(pdf_path)
-
-        # Create a new PDF writer
-        writer = PdfWriter()
-
-        # Add the first page to the writer
-        writer.add_page(reader.pages[0])
-
-        # Write the output to a new file
-        with open(pdf_path, 'wb') as output_file:
-            writer.write(output_file)
 
     def get(self, personal_id, request, queryset, desired_height_cm=3.5):
         original_file_path = "./media/file_templates/DC3 ACTUALIZADO.xlsx"
@@ -411,12 +417,12 @@ class GenerateDC3View(View):
         sheet = wb.active
         sheet.page_setup.paperSize = sheet.PAPERSIZE_LETTER
         sheet.page_margins = PageMargins(
-            top=0.2,
+            top=0,
             left=0.25,
             right=0.01,
             header=0.1,
             footer=0.1,
-            bottom=.2
+            bottom=0
         )
 
         dc3_needed_data = {
@@ -440,7 +446,7 @@ class GenerateDC3View(View):
         }
 
         for personal in queryset:
-            verified_data = self.verify_data(personal, dc3_needed_data)
+            verified_data = verify_data(personal, dc3_needed_data)
 
             cell_mapping = {
                 'AJ5': verified_data['nombre_completo'],
@@ -465,12 +471,12 @@ class GenerateDC3View(View):
 
             if verified_data['logotipo']:
                 img_path = verified_data['logotipo']
-                width, height = self.scale_image_from_height(img_path, desired_height_cm)
+                width, height = scale_image_from_height(img_path, desired_height_cm)
                 self.add_image_to_worksheet(img_path, 'B1', sheet, width, height)
 
             if verified_data['qr_code']:
                 img_path = verified_data['qr_code']
-                width, height = self.scale_image_from_height(img_path, desired_height_cm)
+                width, height = scale_image_from_height(img_path, desired_height_cm)
                 self.add_image_to_worksheet(img_path, 'AC1', sheet, width, height)
 
         wb.save(modified_xlsx_path)
@@ -484,7 +490,7 @@ class GenerateDC3View(View):
         self.convert_xlsx_to_pdf(modified_xlsx_path, pdf_path)
 
         # Keep only the first page on PDF file
-        self.keep_first_page(pdf_path)
+        keep_first_page(pdf_path)
 
         # Return the PDF as a response with the desired filename
         with open(pdf_path, 'rb') as pdf_file:
