100cb233c3c526ebd72fbff0980fe820ac78c694
Jose Luis López López Prieto <ing.jlllopezp@gmail.com>
2023-11-13 08:15:23 -0600

    Added: Code to import a list of Mexican zipcodes to the project

---
+++ main/management/commands/createsuperbossgroup.py
@@ -0,0 +1,17 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
+
+
+class Command(BaseCommand):
+    help = 'Create the Superboss group and assign all permissions to it'
+
+    def handle(self, *args, **options):
+        group_name = 'Superboss'
+        group, created = Group.objects.get_or_create(name=group_name)
+
+        content_types = ContentType.objects.all()
+        permissions = Permission.objects.filter(content_type__in=content_types)
+        group.permissions.set(permissions)
+
+        self.stdout.write(self.style.SUCCESS('Superuser group created successfully!'))
